958ec27039406fef4f50bdcde3ffd29d55fd2b5c
orlyjonathan <168757564+orlyjonathan@users.noreply.github.com>
2024-09-02 19:07:07 +0300

    changes

---
+++ flappyBird/dist/dist/start.dev.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var currentState = "start";
+var collisionCount = 0;
+var maxCollisions = 3;
+window.addEventListener("DOMContentLoaded", function () {
+  var startPage = document.getElementById("startPage");
+  var gamePage = document.getElementById("gamePage");
+  var gameOverPage = document.getElementById("gameOverPage");
+  var startButton = document.getElementById("startButton");
+  var restartButton = document.getElementById("restartButton");
+  var collisionCountDisplay = document.getElementById("collisionCount");
+
+  function render() {
+    if (currentState === "start") {
+      startPage.style.display = "block";
+      gamePage.style.display = "none";
+      gameOverPage.style.display = "none";
+    } else if (currentState === "playing") {
+      startPage.style.display = "none";
+      gamePage.style.display = "block";
+      gameOverPage.style.display = "none";
+    } else if (currentState === "gameOver") {
+      startPage.style.display = "none";
+      gamePage.style.display = "none";
+      gameOverPage.style.display = "block";
+    }
+  }
+
+  startButton.addEventListener("click", function () {
+    currentState = "playing";
+    render();
+    startGame();
+    moveWalls(handleCollision);
+    moveWalls2(handleCollision);
+  });
+  restartButton.addEventListener("click", function () {
+    currentState = "start";
+    restartGame();
+    render();
+  });
+
+  function startGame() {
+    collisionCount = 0;
+    collisionCountDisplay.textContent = collisionCount.toString();
+    simulateCollision();
+  }
+
+  function simulateCollision() {
+    if (currentState == "playing") {
+      collisionCount++;
+      collisionCountDisplay.textContent = collisionCount.toString();
+
+      if (collisionCount >= maxCollisions) {
+        gameOver();
+      } else {
+        //simulate next collision//;
+        setTimeout(function () {
+          simulateCollision();
+        }, 1000);
+      }
+    }
+  }
+
+  function gameOver() {
+    currentState = "gameOver";
+    render();
+  }
+
+  render(); //initial render to show the start page
+});
